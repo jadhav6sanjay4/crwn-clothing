147416683b8d59fb66e57eaf4d21f9117b516243
sanjay jadhav <jadhav6sanjay4@gmail.com>
2023-05-03 18:37:15 +0530

    Firebase changes pushed

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,17 @@
-
+import { signInWithGooglePopup } from "../../utilities/firebase/firebase.utils";
+import { createUserDocumentFromAuth } from "../../utilities/firebase/firebase.utils";
 const SignIn = () => {
+
+    const logGoogleUser = async () => {
+       const response = await signInWithGooglePopup();
+       console.log(response);
+    };
+
     return(
-        <h1>This is sign in page</h1>
+        <div>
+            <h1>Sign In</h1>
+            <button onClick={logGoogleUser}>Sign In With Google PopUp</button>
+        </div>
     );
-}
+};
 export default SignIn;
